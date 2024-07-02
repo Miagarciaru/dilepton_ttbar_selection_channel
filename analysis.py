8abdd96b6d144dbc0b0cd60b5c424b5d1a1138ef
garciarm <miguel.angel.garcia.ruiz@cern.ch>
2024-07-02 03:09:44 +0200

    The new samples for single top is added

--- analysis.py
+++ analysis.py
@@ -4,23 +4,44 @@ import subprocess
 # List of root files to analyze
 
 sample_paths = [
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data15_allyear.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_A.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_B.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_C.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_D.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_E.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_F.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_G.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/data16_allyear_H.root",
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ttbar.root"
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data15_allyear.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_A.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_B.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_C.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_D.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_E.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_F.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_G.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-data-samples/data16_allyear_H.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/ttbar.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410644.PowhegPythia8EvtGen_A14_singletop_schan_lept_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410645.PowhegPythia8EvtGen_A14_singletop_schan_lept_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410659.PhPy8EG_A14_tchan_BW50_lept_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601352.PhPy8EG_tW_dyn_DR_incl_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601353.PhPy8EG_tW_dyn_DR_dil_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601354.PhPy8EG_tW_dyn_DR_dil_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601355.PhPy8EG_tW_dyn_DR_incl_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410658.PhPy8EG_A14_tchan_BW50_lept_top.root"
 ]
 
 """
 sample_paths = [
-    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ttbar.root"
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/ttbar.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410644.PowhegPythia8EvtGen_A14_singletop_schan_lept_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410645.PowhegPythia8EvtGen_A14_singletop_schan_lept_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410659.PhPy8EG_A14_tchan_BW50_lept_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601352.PhPy8EG_tW_dyn_DR_incl_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601353.PhPy8EG_tW_dyn_DR_dil_antitop.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601354.PhPy8EG_tW_dyn_DR_dil_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.601355.PhPy8EG_tW_dyn_DR_incl_top.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410218.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttee.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410219.aMcAtNloPythia8EvtGen_MEN30NLO_A14N23LO_ttmumu.root",
+    "/eos/user/g/garciarm/ntuple-production-samples/data15-16-ttbar/ntuples-mc-samples/mc-samples/mc20_13TeV.410658.PhPy8EG_A14_tchan_BW50_lept_top.root"
 ]
 """
+
 # Root folder for storing output
 output_root_folder = "output_analysis"
 
